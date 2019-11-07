01bfd74e58e21f3216bd33130810672dcb56f8c2
SgtZdog <kevin@zemon.name>
2019-11-07 13:02:45 -0600

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-telnet "0.5.0"
   :description "A clojure telnet client"
-  :url "https://github.com/komcrad/clj-telnet"
+  :url "https://github.com/wsbu/clj-telnet"
   :license {:name "LGPL-3.0"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
