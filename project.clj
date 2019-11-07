5bf521471e6f490840e200af3f031fc6281581c7
SgtZdog <kevin@zemon.name>
2019-11-07 12:50:54 -0600

    [MAN-1720]

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "LGPL-3.0"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [commons-net/commons-net "3.5"]])
+                 [commons-net/commons-net "3.5"]]
+  :deploy-repositories [["releases" {:url "https://mvn.redlion.net/repository/maven-releases"
+                                     :sign-releases false}]
+                        ["snapshots" {:url "https://mvn.redlion.net/repository/maven-snapshots"
+                                      :sign-releases false}]])
